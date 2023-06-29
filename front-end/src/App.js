93b998f97bb9a17207304a7ed418675cc31ce66e
evandrvalente <evandr.valente@gmail.com>
2023-06-29 11:15:08 -0300

    feat: add about section

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import About from './components/about/About';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <main className="main">
+      <Home/>
+      <About/>
+    </main>
     </div>
   );
 }
---
+++ front-end/src/components/about/Info.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      
+      <div className="about__box">
+        <i className="bx bx-award about__icon"></i>
+        <h3 className="about__title">Programando a</h3>
+        <span className="about__subtitle">2 anos</span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Completados</h3>
+        <span className="about__subtitle">38 Projetos</span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bx-support about__icon"></i>
+        <h3 className="about__title">Suporte</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+
+    </div>
+  )
+}
+
+export default Info
